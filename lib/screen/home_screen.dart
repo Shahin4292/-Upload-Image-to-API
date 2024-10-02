b7bb25592d0d28cb21d583b5771c76020305a648
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-02 17:34:47 +0600

    image upload done

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:upload_image_api/service/service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String isImageUpload = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +26,24 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   final ImagePicker picker = ImagePicker();
+                  final XFile? image =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  if (image != null) {
+                    Uint8List bytes = await image.readAsBytes();
+                    UploadApiImage()
+                        .uploadImage(bytes, image.name)
+                        .then((value) {
+                      setState(() {
+                        isImageUpload = value['location'].toString();
+                      });
+                      print(
+                          "Upload Successfully with link ${value.toString()}");
+                    }).onError((error, stackTrace) {
+                      print(error.toString());
+                    });
+                  }
                 },
                 child: const Padding(
                   padding: EdgeInsets.all(12.0),
